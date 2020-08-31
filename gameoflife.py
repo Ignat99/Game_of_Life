779930a73234fa5902d9a8752b38d38664ab7f32
Dimitrii Ignatev <dimitrii@insiktintelligence.com>
2020-08-31 21:42:24 +0200

    Milestone 1 Pylint

--- gameoflife.py
+++ gameoflife.py
@@ -30,39 +30,31 @@ def gol_read():
     gol_j = 0
     for line in sys.stdin:
         result = re.findall(r'^\!', line)
-#        print result
         if result != ['!']:
             gol_i = gol_i + 1
             gol_field_height = gol_i
-#            print(line)
             for gol_char in line:
                 gol_j = gol_j + 1
                 if gol_char == 'O':
-#                   print (gol_i, gol_j)
-                   gol_state.add((gol_i, gol_j))
+                    gol_state.add((gol_i, gol_j))
             gol_field_width = gol_j
             gol_j = 0
     return (gol_field_height, gol_field_width)
 
 def gol_print():
     """ Print ASCII to stdout """
-#    orig_stdout = sys.stdout
     for gol_i in xrange(gol_field_height):
         gol_str = ''
         for gol_j in xrange(gol_field_width-1):
             if (gol_i, gol_j) in gol_state:
-#                print (gol_i, gol_j)
                 gol_str = gol_str + 'O'
             else:
                 gol_str = gol_str + '.'
-#            print (gol_i, gol_j)
-        print(gol_str)
-#        sys.stdout = gol_str + '\n'
-#    sys.stdout = orig_stdout
+        print gol_str
 
 gol_state = set()
-(gol_field_height, gol_field_width) = gol_read()
 #print gol_state
+(gol_field_height, gol_field_width) = gol_read()
 
 for gol_k in range(1):
     gol_state = advance(gol_state)
