d25fafd0332c04e38642d1821d021efa8aaadea5
Dimitrii Ignatev <dimitrii@insiktintelligence.com>
2020-09-03 23:36:21 +0200

    Function Annotations with module typing

--- gameoflife.py
+++ gameoflife.py
@@ -3,18 +3,19 @@
 
 """ Game of life implementation with game variation (Life, Morley, etc)"""
 from __future__ import print_function
+from typing import List, Iterable, Set, Tuple, Generator
 import sys
 import re
 import itertools
 
-def neighbors(point): # Iterable[int]
+def neighbors(point) -> Iterable[Tuple[int, int]]: # Iterable[Tuple[int, int]]
     """ What we have around """
     gol_x, gol_y = point
     for i, j in itertools.product(range(-1, 2), repeat=2):
         if any((i, j)):
             yield (gol_x + i, gol_y + j)
 
-def advance(board, gol_cond_b, gol_cond_s) -> list:
+def advance(board, gol_cond_b, gol_cond_s) -> Set[List[int]]:
     """ Next step. And since only True in the dictionary. Just simply a set of coordinates.
         New state is not needed. We can re-create the list of living cells.
     """
@@ -40,7 +41,7 @@ def advance(board, gol_cond_b, gol_cond_s) -> list:
 
     return newstate
 
-def gol_read() -> list:
+def gol_read() -> Tuple[int, int]:
     """ Read from stdin  new state """
     gol_i = 0
     gol_j = 0
@@ -89,16 +90,16 @@ if __name__ == "__main__":
 
     gol_param_array = sys.argv[1].split('S') # type: List[str]
 
-    gol_state = set() # type: list
-    (gol_field_height, gol_field_width) = gol_read() # type: List[int]
+    gol_state = set() # type: Set
+    (gol_field_height, gol_field_width) = gol_read()
 
     go_s = str(gol_param_array[1]) # type: str
     len_s = len(go_s) # type: int
-    gol_cond_s = gol_cond(go_s, len_s) # type: list
+    gol_cond_s = gol_cond(go_s, len_s)
 
     go_b = str(gol_param_array[0]).split('B')[1] # type: str
     len_b = len(go_b) - 1 # type: int
-    gol_cond_b = gol_cond(go_b, len_b) # type: List[bool]
+    gol_cond_b = gol_cond(go_b, len_b)
 
     for gol_k in range(1):
         gol_state = advance(gol_state, gol_cond_b, gol_cond_s)
